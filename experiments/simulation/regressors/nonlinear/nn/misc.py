5f521fd51418ae9b2a423fc15656f55d2d5036d7
Uzair Akbar <uzair.akbar@tum.de>
2021-04-08 00:17:09 +0200

    Update misc.py

--- experiments/simulation/regressors/nonlinear/nn/misc.py
+++ experiments/simulation/regressors/nonlinear/nn/misc.py
@@ -6,9 +6,9 @@ def plot_loss_graphs(logs, args, train=True, test=True, fig=None, ax=None, scale
     train_logs, test_logs = logs
     fig, ax = plt.subplots(1, 1, figsize=(6, 4))
     x = np.linspace(0, args["epochs"], len(train_logs))
-    ax.plot(x, regr.train_logs)
-    test_log_interp = interp.interp1d(np.arange(len(regr.test_logs)), regr.test_logs)
-    test_log_stretch = test_log_interp(np.linspace(0, len(regr.test_logs)-1, x.size))
+    ax.plot(x, train_logs)
+    test_log_interp = interp.interp1d(np.arange(len(test_logs)), test_logs)
+    test_log_stretch = test_log_interp(np.linspace(0, len(test_logs)-1, x.size))
     if scale == "log":
         ax.set_yscale('log')
     ax.plot(x, test_log_stretch)
