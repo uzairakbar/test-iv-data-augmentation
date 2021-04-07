076ca82fefaa6d4e8a10338a727d69b60a5b82c4
Uzair Akbar <uzair.akbar@tum.de>
2021-04-07 23:27:42 +0200

    Update iv_regression.py

--- experiments/simulation/regressors/linear/iv_regression.py
+++ experiments/simulation/regressors/linear/iv_regression.py
@@ -1,6 +1,6 @@
 import numpy as np
 from sklearn.linear_model import LinearRegression
-from ...data_augmentor import augment_data
+from data_augmentor import augment_data
 
 class IVR(LinearRegression):
     def __init__(self, **kwargs):
