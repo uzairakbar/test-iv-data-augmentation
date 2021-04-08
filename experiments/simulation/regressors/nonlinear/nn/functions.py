105b88f6ff20871f1d1ef3dbf6d15e161622c91b
Uzair Akbar <uzair.akbar@tum.de>
2021-04-08 03:19:44 +0200

    Update functions.py

--- experiments/simulation/regressors/nonlinear/nn/functions.py
+++ experiments/simulation/regressors/nonlinear/nn/functions.py
@@ -5,8 +5,8 @@ def iv_loss(y_pred, y, z, lamda=1.0):
 
     PIz = z.mm( torch.pinverse(z) )
     if lamda < 1.0:
-        batch_size = z.size[0]
-        gamma = np.sqrt(1/1-lamda) - 1.0
+        batch_size = z.size(0)
+        gamma = (1/1-lamda)**0.5 - 1.0
         multiplier = ( torch.eye( batch_size ) + gamma*PIz )
     else:
         multiplier = PIz
