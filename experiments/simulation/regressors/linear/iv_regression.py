3afce35e34ba05404cce63ff341374fdd7660b20
Uzair Akbar <uzair.akbar@tum.de>
2021-04-08 03:21:28 +0200

    Update iv_regression.py

--- experiments/simulation/regressors/linear/iv_regression.py
+++ experiments/simulation/regressors/linear/iv_regression.py
@@ -9,10 +9,10 @@ class IVR(LinearRegression):
     def fit(self, X, y, augment_features = 0, lamda = 1.0, **kwargs):
         X_augmented, g = augment_data(X, augment_features)
         
-        PI = g @ np.linalg.pinv(g)
+        PIz = g @ np.linalg.pinv(g)
         if lamda < 1.0:
             gamma = (1/1-lamda)**0.5 - 1.0
-            Z = ( np.eye( len(X) ) + gamma*PI )
+            multiplier = ( np.eye( len(X) ) + gamma*PIz )
         else:
-            Z = PI
-        return super(IVR, self).fit( Z @ X_augmented, Z @ y)
+            multiplier = PIz
+        return super(IVR, self).fit( multiplier @ X_augmented, multiplier @ y)
