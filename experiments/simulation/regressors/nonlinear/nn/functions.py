22897d255a8a05f1db36b9c861682f25e1be99b3
Uzair Akbar <uzair.akbar@tum.de>
2021-04-08 02:59:50 +0200

    Update functions.py

--- experiments/simulation/regressors/nonlinear/nn/functions.py
+++ experiments/simulation/regressors/nonlinear/nn/functions.py
@@ -3,16 +3,14 @@ from torch.nn import functional as F
 
 def iv_loss(y_pred, y, z, lamda=1.0):
 
-    ### FIGURE THIS OUT!!!
-    # PIz = z.mm( torch.pinverse(z) )
-    # if lamda < 1.0:
-    #     batch_size = z.size[0]
-    #     gamma = np.sqrt(1/1-lamda) - 1.0
-    #     multiplier = ( torch.eye( batch_size ) + gamma*PIz )
-    # else:
-    #     multiplier = PIz
-
-    loss = F.mse_loss(y_pred, y, reduction='mean')
-    # loss = F.mse_loss(multiplier.mm( y_pred ), multiplier.mm(y), reduction=mean)
+    PIz = z.mm( torch.pinverse(z) )
+    if lamda < 1.0:
+        batch_size = z.size[0]
+        gamma = np.sqrt(1/1-lamda) - 1.0
+        multiplier = ( torch.eye( batch_size ) + gamma*PIz )
+    else:
+        multiplier = PIz
+    
+    loss = F.mse_loss(multiplier.mm( y_pred ), multiplier.mm(y), reduction="mean")
 
     return loss
