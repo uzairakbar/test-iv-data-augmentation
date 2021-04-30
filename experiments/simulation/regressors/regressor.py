5e1c20055346ac9968385d698eb29d0ed7508f20
Uzair Akbar <uzair.akbar@tum.de>
2021-04-30 02:06:47 +0200

    Update regressor.py

--- experiments/simulation/regressors/regressor.py
+++ experiments/simulation/regressors/regressor.py
@@ -1,6 +1,7 @@
 from sklearn.base import RegressorMixin
 from .linear.iv_regression import IVR
 from .nonlinear.nn.model import NN
+from .nonlinear.mmriv.mmriv import NN
 
 class Regressor(RegressorMixin):
     def __init__(self, regression = "linear", **kwargs):
@@ -10,12 +11,14 @@ class Regressor(RegressorMixin):
             self.regressor = IVR(**kwargs)
         elif  self.regression == "nn":
             self.regressor = NN(**kwargs)
+        elif  self.regression == "mmriv":
+            parameters = {'kernel':['rbf'],
+              'alpha': alpha_range.rvs(random_state=seed, size=5),
+              'gamma': gamma_range.rvs(random_state=seed, size=5)}
+            self.regressor = GridSearchCV(MMRIV(), parameters)
 
     def fit(self, X, y, augment_features = 0, lamda = 0.0, **kwargs):
-        if self.regression == "linear":
-            return self.regressor.fit(X, y, augment_features, lamda, **kwargs)
-        elif  self.regression == "nn":
-            return self.regressor.fit(X, y, augment_features, lamda, **kwargs)
+        return self.regressor.fit(X, y, augment_features, lamda, **kwargs)
     
     def predict(self, X):
         return self.regressor.predict(X)
